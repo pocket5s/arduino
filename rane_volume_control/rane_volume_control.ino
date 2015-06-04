7a452a2e6af735e15dd3b6e05244ec339a9af785
Robert McIntosh <pocket5s@gmail.com>
2015-06-03 21:08:27 -0500

    added failsafe off

--- rane_volume_control/rane_volume_control.ino
+++ rane_volume_control/rane_volume_control.ino
@@ -1,18 +1,17 @@
 #include <mcp_can.h>
-#include <mcp_can_dfs.h>
 #include <TimerOne.h>
 #include <SPI.h>
-#include "mcp_can.h"
 
 int amps_relay = 4;
 int rane_relay = 5;
 int led_relay = 6;
 int mute_relay = 7;
+MCP_CAN CAN(9);
 
-const byte PWMDAC1pin = 9; // PWM DAC, only pins 9 and 10 are allowed
+const byte PWMDAC1pin = 10; // PWM DAC, only pins 9 and 10 are allowed
 //const byte PWMDAC2pin = 10; // example using second DAC
 const byte period = 32; // for 8 bit DAC 
-
+int no_msg_counter = 0;
 unsigned char Flag_Recv = 0;
 unsigned char len = 0;
 unsigned char buf[8];
@@ -21,6 +20,10 @@ boolean system_on = false;
 boolean leds_on = false;
 boolean muted = false;
 const String HU_OFF = "0-122-2-";
+//const String HU_OFF = "-240-0-";
+//0-13-5-22-16-0-102-2-  HU ON?
+//0-13-5-22-240-0-126-2- Interior lights/guages on
+// HU off       while running?
 const String HU_ON = "0-13-1-22-240-0-";
 const String AUX = "0-76-2-";
 const String VOL_CHANGE = "10-10-10-10-10-3-";
@@ -28,14 +31,18 @@ const String CD_PAUSED = "0-3-5-22-80-0-72-2-";
 const String CD_PLAYING = "0-3-5-22-16-0-72-2-";
 const String CD_TRACK_CHANGE = "0-3-5-22-128-0-72-2-";
 const String RADIO_TRACK_CHANGE = "0-3-5-22-128-0-66-2-";
+// 5-2-13-0-117-0 scroll up track list
+// 5/0-2-24-1-29-0 scroll down track list
 //int outputVolume[] = {0,7,14,21,28,35,42,49,56,63,70,77,84,91,98,105,112,119,126,133,140,147,154,161,168,175,182,189,196,203,210,217,224,231,238,245,250,255};
 
-int outputVolume[] = {0,27,54,81, 108, 135, 162, 189, 216, 243, 270, 297, 324, 351, 378, 405, 432, 459, 486, 513, 540, 567, 594, 621, 648, 675, 702, 729, 756, 783, 810, 837, 864, 891, 918, 945, 972, 999, 1023};
+//int outputVolume[] = {0,27,54,81, 108, 135, 162, 189, 216, 243, 270, 297, 324, 351, 378, 405, 432, 459, 486, 513, 540, 567, 594, 621, 648, 675, 702, 729, 756, 783, 810, 837, 864, 891, 918, 945, 972, 999, 1023};
+int outputVolume[] = {0,10,27,54, 108, 135, 162, 189, 216, 243, 270, 297, 324, 351, 378, 405, 432, 459, 486, 513, 540, 567, 594, 621, 648, 675, 702, 729, 756, 783, 810, 837, 864, 891, 918, 945, 972, 999, 1023};
+
 char str[20];
 
 void setup()
 {
-
+    //pinMode(2, INPUT);
     pinMode(PWMDAC1pin, OUTPUT);  
     pinMode(amps_relay, OUTPUT);
     pinMode(rane_relay, OUTPUT);
@@ -54,19 +61,21 @@ START_INIT:
     {
         Serial.println("CAN BUS Shield init fail");
         Serial.println("Init CAN BUS Shield again");
-        delay(2000);
-        goto START_INIT;
+        delay(4000);
+       
+        goto START_INIT;    
     }
+    
 }
 
 void loop()
 {   
+   
     if(CAN_MSGAVAIL == CAN.checkReceive())            // check if data coming
     {
+        no_msg_counter = 0;
         CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
         String s = "";
-        //char incomingVol = vol;
-        //incomingVol = buf[0];
         for(int i = 0; i<len; i++) {
           s = s + buf[i] + "-";
         }
@@ -76,7 +85,7 @@ void loop()
         else if( s == CD_TRACK_CHANGE ) {
           //Serial.println( "Track change" );
         }
-        else if( s.endsWith( HU_OFF ) ) {
+        else if( s.indexOf( HU_OFF ) > -1 ) {
           // The HU_OFF signal gets sent constantly
           // while it is off, so only flip the switch
           // if needed
@@ -107,7 +116,14 @@ void loop()
         //Serial.println( s );
     }
     else {
-      //Serial.println("No message received");
+      no_msg_counter++;
+      if( no_msg_counter > 10000 ) {
+        if( system_on ) {
+          Serial.println( "No activity, turning off system" );
+          turnSystemOff();
+        }
+        no_msg_counter = 0;
+      }
     }
 }
 
@@ -116,7 +132,6 @@ boolean isVolumeChange( String msg ) {
 }
 
 int parseVolume( String msg ) {
-  //Serial.println( msg );
   int index = msg.indexOf("-");
   int value = msg.substring(0,index).toInt();
   return value;
@@ -132,8 +147,9 @@ void changeVolume( int value ) {
       Serial.print( vol );
       Serial.print( "=" );
       Serial.println( outputVolume[vol] );
-      */
+      /**/
       if( vol == 0 ) {
+        Timer1.pwm(PWMDAC1pin, 1);
         mute(); 
       }
       else {
@@ -145,7 +161,7 @@ void changeVolume( int value ) {
 }
 
 void mute() {
-  digitalWrite( mute_relay, LOW );
+  digitalWrite( mute_relay, HIGH );
   muted = true;
   Serial.println( "Muted" );
 }
@@ -153,7 +169,7 @@ void mute() {
 void unMute() {
   if( muted ) {
     Serial.println( "Un-muting" );
-    digitalWrite( mute_relay, HIGH );
+    digitalWrite( mute_relay, LOW );
     muted = false;
   }
 }
