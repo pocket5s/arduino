bc6157d09a068d519e72c8685f4706a41fd1d09a
Robert McIntosh <pocket5s@gmail.com>
2015-06-25 11:20:47 -0500

    testing updates

--- receive_check_2/receive_check_2.ino
+++ receive_check_2/receive_check_2.ino
@@ -1,21 +1,12 @@
 #include <mcp_can.h>
-#include <mcp_can_dfs.h>
-
-// demo: CAN-BUS Shield, receive data with check mode
-// send data coming to fast, such as less than 10ms, you can use this way
-// loovee, 2013-11-27
-
-
 #include <SPI.h>
-#include "mcp_can.h"
-
 
 unsigned char Flag_Recv = 0;
 unsigned char len = 0;
 unsigned char buf[8];
 char str[20];
 unsigned char previous = 0;
-
+MCP_CAN CAN(9);
 
 void setup()
 {
@@ -52,15 +43,17 @@ void loop()
             //Serial.println( s );
         }
         if( previous == 1 ) {
+           Serial.print( "Previous: " );
            Serial.println( s );
           previous = 0; 
         }
-        if( s.startsWith( "0\t0\t" ) == false &&
+        if( s.startsWith( "0\t0\t40" ) == false &&
             s.endsWith( "0\t0\t1\t" ) == false &&
             s.endsWith( "0\t0\t0\t" ) == false &&
             s.startsWith("31\t111\t96\t") == false &&
             s.startsWith( "253\t" ) == false &&
             s.startsWith( "255\t" ) == false &&
+            s.startsWith( "0\t13\t5" ) == false &&
             s != "3\t" &&
             s != "7\t237\t86\t0\t" &&
             s != "127\t127\t127\t127\t120\t7\t" &&
@@ -92,7 +85,7 @@ s != "31\t111\t111\t255\t16\t0\t0\t93\t" &&
 s != "0\t3\t5\t22\t16\t0\t72\t2\t" &&
 s != "0\t112\t28\t0\t7\t255\t7\t255\t" &&
 s.startsWith( "31\t111\t" ) == false &&
-s.startsWith( "0\t13\t5" ) == false &&
+//s.startsWith( "0\t13\t5" ) == false &&
 s.startsWith( "12\t9\t152" ) == false &&
 s.startsWith( "0\t108\t28" ) == false &&
 s.startsWith( "10\t10\t10" ) == false &&
@@ -102,19 +95,31 @@ s.startsWith( "17\t16" ) == false &&
 s.startsWith( "1\t4\t" ) == false &&
 s.startsWith( "1\t16\t" ) == false &&
 s.startsWith( "10\t10\t10" ) == false &&
+s.startsWith( "0\t109\t") == false &&
+s.startsWith( "0\t50\t") == false &&
+s.startsWith( "2\t169\t") == false &&
+s.startsWith( "2\t193\t") == false &&
+s.startsWith( "2\t") == false &&
+s.startsWith( "0\t128\t") == false &&
+s.startsWith( "1\t84\t") == false &&
+s.startsWith( "12\t97\t") == false &&
 s.startsWith( "5\t10\t10" ) == false &&
-//s.startsWith( "49\t68" ) == false && // 1st
-s.startsWith( "17\t4" ) == false && // after volume
+s.startsWith( "12\t125\t235" ) == false &&
+s.startsWith( "33\t6\t0" ) == false &&
+s.startsWith( "49\t68" ) == false && // 1st
+s.startsWith( "17\t6" ) == false && // after volume
 s.startsWith( "49\t70" ) == false && // after volume
-//s.startsWith( "33\t6" ) == false && // after volume
+s.startsWith( "33\t4" ) == false && // after volume
 s.startsWith( "1\t6" ) == false && // after volume
+s.endsWith( "10\t10\t3\t" ) == false && // volume amount
 s.startsWith("0\t1\t1\t0\t") == false // don't show track #
+
 
             ) {
         
-              if( s == "0\t3\t5\t22\t128\t0\t72\t2\t" ) {
+              if( s == "17\t4\t0\t101\t0\t116\t0\t111\t" ) {
                 previous = 1;
-                Serial.println( "Set previous" );
+                //Serial.println( "Set previous" );
               }
                   Serial.println( s );
                   //Serial.println();
